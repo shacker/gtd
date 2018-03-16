b3327878f43103d3cb0ce6e62375c22a107cff38
Scot Hacker <shacker@birdhouse.org>
2018-03-15 23:20:24 -0700

    Misc tweaks

---
+++ project/local.example.py
@@ -0,0 +1,19 @@
+# Overrides
+from .settings import *  # noqa: F401
+
+SECRET_KEY = 'lksdf98wrhkjs88dsf8-324ksdm'
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gtd',
+        'USER': 'you',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    },
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
