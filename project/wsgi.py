4dc7fef8c51fc365e940cce8115b5095c6005590
Scot Hacker <shacker@birdhouse.org>
2018-03-31 09:33:04 -0700

    Tweak wsgi path to local settings

--- project/wsgi.py
+++ project/wsgi.py
@@ -3,7 +3,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.local')
 log = logging.getLogger(__name__)
 
 application = get_wsgi_application()
