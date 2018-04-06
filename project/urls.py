4957b3204065a33a699a6c513bfcdfb3407a5d92
Scot Hacker <shacker@birdhouse.org>
2018-04-05 22:35:15 -0700

    Update pipfile

--- project/urls.py
+++ project/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
-    path('admin/', admin.site.urls),
+    path('gtdadmin/', admin.site.urls),
     path('lists/', include('todo.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # Static media in DEBUG mode
