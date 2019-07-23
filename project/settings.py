33ea03b7b0848fa3dd33db770f639c6ca74f2062
Scot Hacker <shacker@birdhouse.org>
2019-07-22 23:32:29 -0700

    Update Django to 2.2.3 and running version of todo

--- project/settings.py
+++ project/settings.py
@@ -12,11 +12,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = "America/Los_Angeles"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
 SITE_ID = 1
 
@@ -24,49 +24,48 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-LOGIN_URL = "/login"
-LOGIN_REDIRECT_URL = 'home'
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'project.urls'
-LOGIN_REDIRECT_URL = 'todo:lists'
-LOGOUT_REDIRECT_URL = 'home'
+ROOT_URLCONF = "project.urls"
+LOGIN_URL = "/login"
+LOGIN_REDIRECT_URL = "todo:lists"
+LOGOUT_REDIRECT_URL = "home"
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_SECURITY_WARN_AFTER = 5
 SESSION_SECURITY_EXPIRE_AFTER = 12
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = "project.wsgi.application"
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.flatpages',
-    'django.contrib.messages',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.staticfiles',
-    'todo',
-    'django_extensions',
+    "django.contrib.admin",
+    "django.contrib.admindocs",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.flatpages",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.staticfiles",
+    "todo",
+    "django_extensions",
 )
 
 # Static files and uploads
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project', 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "project", "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Uploaded media
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -79,27 +78,27 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 # Override in local.py :
 # ######################
 
-SECRET_KEY = ''
+SECRET_KEY = ""
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'project', 'templates'), ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "project", "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
                 # Your stuff: custom template context processors go here
-            ],
+            ]
         },
-    },
+    }
 ]
 
 
@@ -110,9 +109,9 @@ MESSAGE_TAGS = {message_constants.ERROR: "danger"}
 DATABASES = {}
 
 # Todo-specific settings
-TODO_STAFF_ONLY = False
-TODO_DEFAULT_LIST_ID = None
-TODO_DEFAULT_ASSIGNEE = None
-TODO_PUBLIC_SUBMIT_REDIRECT = '/'
-TODO_ALLOW_FILE_ATTACHMENTS = True
-TODO_LIMIT_FILE_ATTACHMENTS = [".jpg", ".gif", ".png", ".csv", ".pdf"]
+# TODO_STAFF_ONLY = False
+# TODO_DEFAULT_LIST_ID = None
+# TODO_DEFAULT_ASSIGNEE = None
+# TODO_PUBLIC_SUBMIT_REDIRECT = '/'
+# TODO_ALLOW_FILE_ATTACHMENTS = True
+# TODO_LIMIT_FILE_ATTACHMENTS = [".jpg", ".gif", ".png", ".csv", ".pdf"]
