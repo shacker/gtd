dcb7cde0622cc5ba5558b50fb683108c6cfb7b90
Scot Hacker <shacker@birdhouse.org>
2023-06-10 23:03:11 -0700

    Prep for fly.io

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-ARG PYTHON_VERSION=3.10-slim-buster
+ARG PYTHON_VERSION=3.11-slim-buster
 
 FROM python:${PYTHON_VERSION}
 
@@ -10,17 +10,14 @@ RUN mkdir -p /code
 WORKDIR /code
 
 COPY requirements.txt /tmp/requirements.txt
-
 RUN set -ex && \
     pip install --upgrade pip && \
     pip install -r /tmp/requirements.txt && \
     rm -rf /root/.cache/
+COPY . /code
 
-COPY . /code/
-
-# This is in the default Fly.io Dockerfile, but doesn't work with the current
-#   workflow used by simple_deploy.
-# RUN python manage.py collectstatic --noinput
+ENV SECRET_KEY "rlplICcno4oth2GlUgdQoUDEXlJ1m1eIixwhSWOwwGioqcCPLH"
+RUN python manage.py collectstatic --noinput
 
 EXPOSE 8000
 
--- project/settings.py
+++ project/settings.py
@@ -25,6 +25,7 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
+ALLOWED_HOSTS = ["*"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -120,29 +121,3 @@ DATABASES = {}
 # TODO_ALLOW_FILE_ATTACHMENTS = True
 # TODO_LIMIT_FILE_ATTACHMENTS = [".jpg", ".gif", ".png", ".csv", ".pdf"]
 # TODO_GROUPS_MODEL = ""
-
-
-
-# --- Settings for Fly.io. ---
-import os
-
-if os.environ.get("ON_FLYIO"):
-    import dj_database_url
-    
-    # Use secret, if set, to update DEBUG value.
-    if os.environ.get("DEBUG") == "FALSE":
-        DEBUG = False
-    elif os.environ.get("DEBUG") == "TRUE":
-        DEBUG = True
-
-    # Set a Fly.io-specific allowed host.
-    # ALLOWED_HOSTS.append('icy-lake-4520.fly.dev')
-    # Using '*' while further troubleshooting CSRF issue.
-    ALLOWED_HOSTS.append('icy-lake-4520.fly.dev')
-
-    # Use the Fly.io Postgres database.
-    db_url = os.environ.get("DATABASE_URL")
-    DATABASES['default'] = dj_database_url.parse(db_url)
-
-    # Prevent CSRF "Origin checking failed" issue.
-    CSRF_TRUSTED_ORIGINS = ['https://icy-lake-4520.fly.dev']
